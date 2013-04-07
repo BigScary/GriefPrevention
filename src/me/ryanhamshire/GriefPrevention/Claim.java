1de19e5629a1adbe5b8917a2c911dd3846afb42c
Matej <Matej@KOMP>
2013-04-07 21:26:26 +0200

    API: Ability to access list of all claims and ability to access both points of claim.

--- src/me/ryanhamshire/GriefPrevention/Claim.java
+++ src/me/ryanhamshire/GriefPrevention/Claim.java
@@ -39,8 +39,8 @@ public class Claim
 {
 	//two locations, which together define the boundaries of the claim
 	//note that the upper Y value is always ignored, because claims ALWAYS extend up to the sky
-	Location lesserBoundaryCorner;
-	Location greaterBoundaryCorner;
+	public Location lesserBoundaryCorner;
+	public Location greaterBoundaryCorner;
 	
 	//modification date.  this comes from the file timestamp during load, and is updated with runtime changes
 	public Date modifiedDate;
