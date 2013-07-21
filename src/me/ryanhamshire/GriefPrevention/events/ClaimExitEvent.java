3d04a2aa379b61b2daaf96de268a4a05767e717f
BC_Programming <bc_programming@hotmail.com>
2013-07-20 18:20:24 -0700

    added missing event base classes.

---
+++ src/me/ryanhamshire/GriefPrevention/events/ClaimExitEvent.java
@@ -0,0 +1,22 @@
+package me.ryanhamshire.GriefPrevention.events;
+
+import me.ryanhamshire.GriefPrevention.Claim;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+public class ClaimExitEvent extends PlayerClaimEvent{
+    private static final HandlerList handlers = new HandlerList();
+    
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+     
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    public ClaimExitEvent(Claim c,Player p){
+    	super(c,p);
+    }
+
+}
