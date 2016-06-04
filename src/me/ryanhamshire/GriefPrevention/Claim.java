943dde60a37898c5ee6b3ea12b47963076ffae6a
ryanhamshire <ryanhamshire@live.com>
2016-06-03 20:35:27 -0700

    Made beetroot farmable with /ContainerTrust.

--- src/me/ryanhamshire/GriefPrevention/Claim.java
+++ src/me/ryanhamshire/GriefPrevention/Claim.java
@@ -313,7 +313,8 @@ public class Claim
 	        Material.MELON_STEM,
 	        Material.CARROT,
 	        Material.POTATO,
-	        Material.NETHER_WARTS);
+	        Material.NETHER_WARTS,
+	        Material.BEETROOT_BLOCK);
 	    
     private boolean placeableForFarming(Material material)
     {
