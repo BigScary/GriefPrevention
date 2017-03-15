f800d76e9e54672f30daf20988080cee7e0fe610
RoboMWM <robomwm@users.noreply.github.com>
2017-03-15 13:34:10 -0700

    darnit intellij, you dropped my work!!!!

--- src/me/ryanhamshire/GriefPrevention/events/custom/GPAlterEvent.java
+++ src/me/ryanhamshire/GriefPrevention/events/custom/GPAlterEvent.java
@@ -2,6 +2,17 @@ package me.ryanhamshire.GriefPrevention.events.custom;
 
 /**
  * Created on 3/11/2017.
+ * Called when a block is "altered."
+ *
+ * Includes:
+ * Item frame (rotation)
+ * Changing redstone settings
+ * Causing crops to be uprooted
+ *
+ * The following are considered "access" alterations:
+ * Opening doors
+ * Opening trap doors
+ * Toggling levers, pressing buttons
  *
  * @author RoboMWM
  */
--- src/me/ryanhamshire/GriefPrevention/events/custom/GPTheftEvent.java
+++ src/me/ryanhamshire/GriefPrevention/events/custom/GPTheftEvent.java
@@ -3,6 +3,10 @@ package me.ryanhamshire.GriefPrevention.events.custom;
 /**
  * Created on 3/10/2017.
  *
+ * Called when a player is:
+ * - accessing an inventory
+ * - moving items with a fishing rod
+ * 
  * @author RoboMWM
  */
 public class GPTheftEvent
