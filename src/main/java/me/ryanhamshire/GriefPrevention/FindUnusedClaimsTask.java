BigScary/GriefPrevention

commit 49d51a34a3999c0ea0d0780e572bc6d6ce31dc7c

Randomize unused claim check (#736)

src/main/java/me/ryanhamshire/GriefPrevention/FindUnusedClaimsTask.java
@@ -15,13 +15,15 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
- package me.ryanhamshire.GriefPrevention;
 
-import java.util.HashSet;
+package me.ryanhamshire.GriefPrevention;
+
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.Set;
+import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
 
@@ -33,7 +35,7 @@ import org.bukkit.Bukkit;
 //runs every 1 minute in the main thread
 class FindUnusedClaimsTask implements Runnable 
 {
-	private Set<UUID> claimOwnerUUIDs = new HashSet<>();
+	private List<UUID> claimOwnerUUIDs;
 	private Iterator<UUID> claimOwnerIterator;
 	
 	FindUnusedClaimsTask()
@@ -57,12 +59,16 @@ class FindUnusedClaimsTask implements Runnable
 		Bukkit.getScheduler().runTaskAsynchronously(GriefPrevention.instance, new CleanupUnusedClaimPreTask(claimOwnerIterator.next()));
 	}
 
-	public void refreshUUIDs()
-	{
-		claimOwnerUUIDs.clear();
-		for (Claim claim : GriefPrevention.instance.dataStore.claims)
-			claimOwnerUUIDs.add(claim.ownerID);
-		claimOwnerUUIDs.remove(null);
+	public void refreshUUIDs() {
+		// Fetch owner UUIDs from list of claims
+		claimOwnerUUIDs = GriefPrevention.instance.dataStore.claims.stream().filter(Objects::nonNull)
+						.distinct().map(claim -> claim.ownerID).collect(Collectors.toList());
+
+		if (!claimOwnerUUIDs.isEmpty()) {
+			// Randomize order
+			Collections.shuffle(claimOwnerUUIDs);
+		}
+
 		claimOwnerIterator = claimOwnerUUIDs.iterator();
 	}
 }

commit e0a0be7befb9d32859f334647a944c086346a412

Move null check to the correct location (#746)
Closes #744, bug introduced in #736

src/main/java/me/ryanhamshire/GriefPrevention/FindUnusedClaimsTask.java
@@ -18,12 +18,10 @@
 
 package me.ryanhamshire.GriefPrevention;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -63,10 +61,8 @@ class FindUnusedClaimsTask implements Runnable
 
 	public void refreshUUIDs() {
 		// Fetch owner UUIDs from list of claims
-		Set<UUID> uuidSet = GriefPrevention.instance.dataStore.claims.stream().filter(Objects::nonNull)
-						.distinct().map(claim -> claim.ownerID).collect(Collectors.toSet());
-
-		claimOwnerUUIDs = new ArrayList<>(uuidSet);
+		claimOwnerUUIDs = GriefPrevention.instance.dataStore.claims.stream().map(claim -> claim.ownerID)
+						.distinct().filter(Objects::nonNull).collect(Collectors.toList());
 
 		if (!claimOwnerUUIDs.isEmpty()) {
 			// Randomize order